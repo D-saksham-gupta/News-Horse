dce2364e1c49bce37fd1a6a2747759b41d0768f6
D-saksham-gupta <sakshamprem2003@gmail.com>
2021-09-19 01:26:22 +0530

    Version 3

--- src/components/News.js
+++ src/components/News.js
@@ -1,88 +1,101 @@
-import React, {useEffect, useState} from 'react'
-import NewsItem from './NewsItem'
-import Spinner from './Spinner';
-import PropTypes from 'prop-types'
+import React, { useEffect, useState } from "react";
+import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from "prop-types";
 
+const News = (props) => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [totalResults, setTotalResults] = useState(0);
+  // document.title= `NewsHorse - ${capitalizeFirst(props.category)}`;
+  const capitalizeFirst = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  };
 
+  const updateNews = async () => {
+    props.setProgress(10);
+    let url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7ea07832261740bfbe0e2e5386fff230&page=${page}&pageSize=${props.pageSize}`;
 
-const News = (props)=> {
+    setLoading(true);
+    let data = await fetch(url);
+    props.setProgress(30);
+    let parsedData = await data.json();
+    props.setProgress(60);
+    setArticles(parsedData.articles);
+    setTotalResults(parsedData.totalResults);
+    props.setProgress(80);
+    setLoading(false);
+    props.setProgress(100);
+  };
+  useEffect(() => {
+    updateNews();
+  }, []);
+  const handlePre = async () => {
+    setPage(page - 1);
+    updateNews();
+  };
+  const handleNext = async () => {
+    setPage(page + 1);
+    updateNews();
+  };
 
-    const [articles, setArticles] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [page, setPage] = useState(1)
-    const [totalResults, setTotalResults] = useState(0)
-    // document.title= `NewsHorse - ${capitalizeFirst(props.category)}`;
-     const capitalizeFirst = (string)=>{
-          return string.charAt(0).toUpperCase() + string.slice(1);
-      }
-    
-    
-   const updateNews = async() => {
-       props.setProgress(10);
-        let url= `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=7ea07832261740bfbe0e2e5386fff230&page=${page}&pageSize=${props.pageSize}`;
-        
-        setLoading(true);
-        let data = await fetch(url);
-        props.setProgress(30);
-        let parsedData = await data.json()
-        props.setProgress(60);
-        setArticles(parsedData.articles);
-        setTotalResults(parsedData.totalResults);
-        setLoading(false);
-        props.setProgress(80);
-        props.setProgress(100);
-    }
-    useEffect(() => {
-        updateNews();
-         
-        }
-    , [])
-       const handlePre= async ()=>{
-           
-           setPage(page - 1);
-           updateNews();
-            
-        }
-        const handleNext= async ()=>{
-           
-            setPage(page + 1);
-            updateNews();
-        }
-        
-        
-
-    
-        return (
-            <div className="container my-3">
-                <h1 className="text-center my-3">NewsHorse - Top Headlines from {capitalizeFirst(props.category)} News</h1>
-               {loading && <Spinner/>}
-                <div className="row">
-                {!loading && articles.map((element) =>{
-                    return <div className="col-md-4" key={element.url}>
-                <NewsItem title = {element.title?element.title:""} description={element.description?element.description:""} imgUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
-                </div>
-                })}
-                   
-                   
-               
-                </div>
-                <div className="container d-flex justify-content-between">
-                <button  disabled={page<=1} type="button" className="btn btn-dark" style={{backgroundColor:'#7952b3'}} onClick={handlePre}>&larr; Previous</button>
-                <button disabled={page + 1 > Math.ceil(totalResults/props.pageSize)} type="button" className="btn btn-dark" style={{backgroundColor:'#7952b3'}}  onClick={handleNext}>Next &rarr;</button>
-                </div>
-                
-            </div>
-        )
-    }
+  return (
+    <div className="container my-3">
+      <h1 className="text-center my-3">
+        NewsHorse - Top Headlines from {capitalizeFirst(props.category)} News
+      </h1>
+      {loading && <Spinner />}
+      <div className="row">
+        {!loading &&
+          articles.map((element) => {
+            return (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imgUrl={element.urlToImage}
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            );
+          })}
+      </div>
+      <div className="container d-flex justify-content-between">
+        <button
+          disabled={page <= 1}
+          type="button"
+          className="btn btn-dark"
+          style={{ backgroundColor: "#7952b3" }}
+          onClick={handlePre}
+        >
+          &larr; Previous
+        </button>
+        <button
+          disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}
+          type="button"
+          className="btn btn-dark"
+          style={{ backgroundColor: "#7952b3" }}
+          onClick={handleNext}
+        >
+          Next &rarr;
+        </button>
+      </div>
+    </div>
+  );
+};
 
 News.defaultProps = {
-    country: 'in',
-    pageSize: 6,
-    category: 'general'
-  }
+  country: "in",
+  pageSize: 6,
+  category: "general",
+};
 News.propTypes = {
-    country: PropTypes.string,
-    pageSize:PropTypes.number,
-    category: PropTypes.string,
-  }
-export default News
+  country: PropTypes.string,
+  pageSize: PropTypes.number,
+  category: PropTypes.string,
+};
+export default News;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,23 +1,46 @@
-import React from 'react'
+import React from "react";
 
 const NewsItem = (props) => {
-    
-    
-        let {title, description, imgUrl, newsUrl, author, date, source} = props;
-        return (
-            <div className="my-3">
-                <div className="card">
-  <img src={!imgUrl?"https://www.plasticstoday.com/sites/plasticstoday.com/files/styles/article_featured_retina/public/FTR-Breaking-News-AdobeStock_397269312.jpg?itok=eZwalMlV":imgUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title} <span className="badge rounded-pill bg-warning text-dark">{source}</span></h5>
-    <p className="card-text">{description}</p>
-    <p className="card-text"><small className="text-info">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
-    <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark" style={{backgroundColor:'#7952b3'}}>Read More</a>
-  </div>
-</div>  
-            </div>
-        )
-    
-}
+  let { title, description, imgUrl, newsUrl, author, date, source } = props;
+  return (
+    <div className="my-3">
+      <div className="card">
+        <img
+          src={
+            !imgUrl
+              ? "https://www.plasticstoday.com/sites/plasticstoday.com/files/styles/article_featured_retina/public/FTR-Breaking-News-AdobeStock_397269312.jpg?itok=eZwalMlV"
+              : imgUrl
+          }
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">
+            {title}{" "}
+            <span className="badge rounded-pill bg-warning text-dark">
+              {source}
+            </span>
+          </h5>
+          <p className="card-text">{description}</p>
+          <p className="card-text">
+            <small className="text-info">
+              By {!author ? "Unknown" : author} on{" "}
+              {new Date(date).toGMTString()}
+            </small>
+          </p>
+          <a
+            href={newsUrl}
+            rel="noreferrer"
+            target="_blank"
+            className="btn btn-sm btn-dark"
+            style={{ backgroundColor: "#7952b3" }}
+          >
+            Read More
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NewsItem
+export default NewsItem;
